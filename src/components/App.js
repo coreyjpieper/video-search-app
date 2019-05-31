2ff8ffd31c009340421ae703d5fb64dac29e15ea
Corey <coreyjpieper@gmail.com>
2019-05-30 23:00:51 -0500

    Basic app ✨

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+        Application
+        <div />
+        React
+        <div />
+        More text
+      </div>
+    );
+  }
+}
+
+export default App;
